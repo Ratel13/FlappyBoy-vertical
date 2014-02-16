1ec4ca140f26eca6bc6ab72d2920ef93393dd62c
yuye-liu <yuye.liu@chukong-inc.com>
2014-02-16 16:37:39 +0800

    init vertical project

---
+++ src/myApp.js
@@ -0,0 +1,109 @@
+/****************************************************************************
+ Copyright (c) 2010-2012 cocos2d-x.org
+ Copyright (c) 2008-2010 Ricardo Quesada
+ Copyright (c) 2011      Zynga Inc.
+
+ http://www.cocos2d-x.org
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ****************************************************************************/
+
+var Helloworld = cc.Layer.extend({
+    isMouseDown:false,
+    helloImg:null,
+    helloLabel:null,
+    circle:null,
+    sprite:null,
+
+    init:function () {
+        //////////////////////////////
+        // 1. super init first
+        this._super();
+
+        /////////////////////////////
+        // 2. add a menu item with "X" image, which is clicked to quit the program
+        //    you may modify it.
+        // ask director the window size
+        var size = cc.Director.getInstance().getWinSize();
+
+        var lazyLayer = cc.Layer.create();
+        this.addChild(lazyLayer);
+
+        this.sprite = cc.Sprite.create(s_startBg);
+        this.sprite.setPosition(size.width / 2, size.height / 2);
+        lazyLayer.addChild(this.sprite, 0);
+        this.setTouchEnabled(true);
+/*
+        sys.localStorage.setItem('bestScore',0);
+
+        sys.localStorage.setItem('secondScore',0);
+    */
+        var startItem = cc.MenuItemImage.create
+        (
+            s_startBtn1,
+            s_startBtn2,
+            function ()
+            {
+                var nextScene = cc.Scene.create();
+                var nextLayer = new GameScene;
+                nextScene.addChild(nextLayer);
+                cc.Director.getInstance().replaceScene(cc.TransitionSlideInT.create(0.0, nextScene));
+            },
+            this
+        );
+
+        var menu = cc.Menu.create(startItem);
+        menu.setPosition(0,0);
+        this.addChild(menu, 1);
+        startItem.setPosition(size.width / 2, size.height / 4);
+
+
+        return true;
+    },
+    // a selector callback
+    menuCloseCallback:function (sender) {
+        cc.Director.getInstance().end();
+    },
+    onTouchesBegan:function (touches, event) {
+        this.isMouseDown = true;
+    },
+    onTouchesMoved:function (touches, event) {
+        if (this.isMouseDown) {
+            if (touches) {
+                //this.circle.setPosition(touches[0].getLocation().x, touches[0].getLocation().y);
+            }
+        }
+    },
+    onTouchesEnded:function (touches, event) {
+        this.isMouseDown = false;
+    },
+    onTouchesCancelled:function (touches, event) {
+        console.log("onTouchesCancelled");
+    }
+});
+
+var HelloWorldScene = cc.Scene.extend({
+    onEnter:function () {
+        this._super();
+        var layer = new Helloworld();
+        layer.init();
+        this.addChild(layer);
+    }
+});
+
